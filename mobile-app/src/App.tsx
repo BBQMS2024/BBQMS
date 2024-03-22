326269368cd7d7b5caee7e58d115d75b3a8ab089
emiragovic <eagovic1@etf.unsa.ba>
2024-03-22 01:38:16 +0100

    change app entry, add constants

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
 
 export default function App() {
   return (
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+registerRootComponent(App);
