62c3189d7889a99e36e4a6015b0cb2aad67680d6
ebabic1 <ebabic1@etf.unsa.ba>
2024-03-23 17:06:07 +0100

    remove entities and repositories

--- backend/src/main/java/ba/unsa/etf/si/bbqms/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.si.bbqms.repository;
-
-import ba.unsa.etf.si.bbqms.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- backend/src/main/java/ba/unsa/etf/si/bbqms/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.si.bbqms.repository;
-
-import ba.unsa.etf.si.bbqms.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
