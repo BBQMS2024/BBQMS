4c89d9dc16630b312d2dd495ced353a291234b21
ebabic1 <ebabic1@etf.unsa.ba>
2024-03-24 02:16:16 +0100

    implement PR requested changes

--- backend/src/main/java/ba/unsa/etf/si/bbqms/ws/models/ErrorResponseDto.java
+++ backend/src/main/java/ba/unsa/etf/si/bbqms/ws/models/ErrorResponseDto.java
@@ -1,13 +1,3 @@
 package ba.unsa.etf.si.bbqms.ws.models;
 
-public class ErrorResponseDto {
-    private final String message;
-
-    public ErrorResponseDto(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-}
+public record ErrorResponseDto (String message) {}
