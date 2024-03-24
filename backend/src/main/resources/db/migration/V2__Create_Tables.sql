c70e233fa765d464a596e497b6be110e41857256
ebabic1 <ebabic1@etf.unsa.ba>
2024-03-24 16:22:29 +0100

    implement requested changes for PR #2

--- backend/src/main/resources/db/migration/V2__Create_Tables.sql
+++ backend/src/main/resources/db/migration/V2__Create_Tables.sql
@@ -3,12 +3,12 @@ BEGIN;
 CREATE TABLE tenant
 (
     id              BIGINT AUTO_INCREMENT NOT NULL,
-    code            VARCHAR(255)          NULL,
+    code            VARCHAR(255)          NOT NULL,
     name            VARCHAR(255)          NULL,
     hq_address      VARCHAR(255)          NULL,
-    font            VARCHAR(255)          NULL,
-    welcome_message VARCHAR(255)          NULL,
-    logo_id         BIGINT                NULL,
+    font            VARCHAR(255)          NOT NULL,
+    welcome_message VARCHAR(255)          NOT NULL,
+    logo_id         BIGINT                NOT NULL,
     CONSTRAINT pk_tenant PRIMARY KEY (id)
 );
 
@@ -21,6 +21,9 @@ CREATE TABLE tenant_logo
 
 ALTER TABLE tenant
     ADD CONSTRAINT uc_tenant_logo UNIQUE (logo_id);
+
+ALTER TABLE tenant
+    ADD CONSTRAINT uc_tenant_code UNIQUE (code);
 
 ALTER TABLE tenant
     ADD CONSTRAINT fk_tenant_on_logo FOREIGN KEY (logo_id) REFERENCES tenant_logo (id);
