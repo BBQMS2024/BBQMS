5f379ac8bbc36443a9ae02635aefcd5da81a0370
Emina Efendić <115942770+efendicemina@users.noreply.github.com>
2024-03-24 17:21:33 +0100

    Adding login auth component

---
+++ admin-app/src/components/LoginAuth/LoginAuth.jsx
@@ -0,0 +1,203 @@
+import React, { useEffect, useRef, useReducer } from "react";
+import "./LoginAuth.css";
+
+function doSubmit(submittedValues) {
+    console.log(`Submitted: ${submittedValues.join("")}`);
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, 1500);
+    });
+}
+
+function clampIndex(index) {
+    if (index > 6) {
+        return 6;
+    } else if (index < 0) {
+        return 0;
+    } else {
+        return index;
+    }
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "INPUT":
+            return {
+                ...state,
+                inputValues: [
+                    ...state.inputValues.slice(0, action.payload.index),
+                    action.payload.value,
+                    ...state.inputValues.slice(action.payload.index + 1)
+                ],
+                focusedIndex: clampIndex(state.focusedIndex + 1)
+            };
+
+        case "BACK":
+            return {
+                ...state,
+                focusedIndex: clampIndex(state.focusedIndex - 1)
+            };
+
+        case "PASTE":
+            return {
+                ...state,
+                inputValues: state.inputValues.map(
+                    (_, index) => action.payload.pastedValue[index] || ""
+                )
+            };
+
+        case "FOCUS":
+            return {
+                ...state,
+                focusedIndex: action.payload.focusedIndex
+            };
+
+        case "VERIFY":
+            return {
+                ...state,
+                status: "pending"
+            };
+
+        case "VERIFY_SUCCESS":
+            return {
+                ...state,
+                status: "idle"
+            };
+
+        default:
+            throw new Error("unknown action");
+    }
+}
+
+const initialState = {
+    inputValues: Array(6).fill(""),
+    focusedIndex: 0,
+    status: "idle"
+};
+
+export default function App() {
+    const [{ inputValues, focusedIndex, status }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
+    // console.log(focusedIndex);
+
+    function handleInput(index, value) {
+        dispatch({ type: "INPUT", payload: { index, value } });
+    }
+
+    function handleBack() {
+        dispatch({ type: "BACK" });
+    }
+
+    function handlePaste(pastedValue) {
+        dispatch({ type: "PASTE", payload: { pastedValue } });
+
+        if (pastedValue.length === 6) {
+            dispatch({ type: "VERIFY" });
+            doSubmit(pastedValue.split("")).then(() =>
+                dispatch({ type: "VERIFY_SUCCESS" })
+            );
+        }
+    }
+
+    function handleFocus(focusedIndex) {
+        dispatch({ type: "FOCUS", payload: { focusedIndex } });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        dispatch({ type: "VERIFY" });
+        doSubmit(inputValues).then(() => dispatch({ type: "VERIFY_SUCCESS" }));
+    }
+
+    return (
+        <div id="auth-container">
+        <form id="authForm" onSubmit={handleSubmit}>
+            <div className="inputs">
+                {inputValues.map((value, index) => {
+                    return (
+                        <Input
+                            key={index}
+                            index={index}
+                            value={value}
+                            onChange={handleInput}
+                            onBackspace={handleBack}
+                            onPaste={handlePaste}
+                            isFocused={index === focusedIndex}
+                            onFocus={handleFocus}
+                            isDisabled={status === "pending"}
+                        />
+                    );
+                })}
+            </div>
+            <button disabled={status === "pending"}>
+                {status === "pending" ? "VERIFYING..." : "VERIFY"}
+            </button>
+        </form>
+        </div>
+    );
+}
+
+function Input({
+                   index,
+                   value,
+                   onChange,
+                   onPaste,
+                   onBackspace,
+                   isFocused,
+                   onFocus,
+                   isDisabled
+               }) {
+    const ref = useRef();
+    useEffect(() => {
+        requestAnimationFrame(() => {
+            // console.log(
+            //   ref.current,
+            //   document.activeElement,
+            //   ref.current !== document.activeElement
+            // );
+            if (ref.current !== document.activeElement && isFocused) {
+                ref.current.focus();
+            }
+        });
+    }, [isFocused]);
+
+    function handleChange(e) {
+        onChange(index, e.target.value);
+    }
+
+    function handlePaste(e) {
+        onPaste(e.clipboardData.getData("text"));
+    }
+
+    function handleKeyDown(e) {
+        if (e.key === "Backspace") {
+            onBackspace();
+        }
+    }
+
+    function handleFocus(e) {
+        e.target.setSelectionRange(0, 1);
+        onFocus(index);
+    }
+
+    return (
+        <input
+            ref={ref}
+            type="text"
+            value={value}
+            onChange={handleChange}
+            onPaste={handlePaste}
+            onKeyDown={handleKeyDown}
+            maxLength="1"
+            onFocus={handleFocus}
+            disabled={isDisabled}
+        />
+    );
+}
+
+
--- admin-app/src/components/LoginForm/LoginForm.jsx
+++ admin-app/src/components/LoginForm/LoginForm.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import validator from "validator";
 import "./LoginForm.css";
+import LoginAuth from "../LoginAuth/LoginAuth";
+import {Route, Routes} from "react-router-dom";
+
 
 const LoginForm = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const [isSubmitted, setIsSubmitted] = useState(false); // Dodali smo novo stanje
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -25,6 +29,10 @@ const LoginForm = () => {
             return;
         }
 
+        // Ako sve provere prođu, postavljamo isSubmitted na true
+        setIsSubmitted(true);
+
+        // Clear form fields i error ako je sve u redu
         setUsername("");
         setPassword("");
         setError("");
@@ -40,6 +48,14 @@ const LoginForm = () => {
         setError("");
     };
 
+    if (isSubmitted){
+        return (
+            <Routes>
+                <Route path='/' element={ <LoginAuth /> } />
+            </Routes>)
+    }
+
+    // Inače prikaži formu za login
     return (
         <div id="login-form">
             <h1>LOGIN</h1>
@@ -74,5 +90,6 @@ const LoginForm = () => {
         </div>
     );
 };
+
 
 export default LoginForm;
