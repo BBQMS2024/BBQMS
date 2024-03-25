bc6910fd8b75f301aabff7ffa24548bca4ce625d
emiragovic <eagovic1@etf.unsa.ba>
2024-03-25 21:47:01 +0100

    change app entry point, add packages

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,23 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import AppNavigator from "./navigation/AppNavigator";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return AppNavigator();
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 registerRootComponent(App);
