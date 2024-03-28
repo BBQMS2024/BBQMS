745ccab0b85d5927ec7ecffa449b6787dc7d548e
Emina Efendić <115942770+efendicemina@users.noreply.github.com>
2024-03-28 11:46:43 +0100

    Fixed bugs

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -1,16 +1,30 @@
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import LoginScreen from './pages/LoginScreen/LoginScreen';
-import { Route, Routes } from 'react-router-dom';
-import RegistrationScreen from './pages/RegistrationScreen/RegistrationScreen.jsx';
-import CompanyDetailsForm from "./components/CompanyDetailsForm/CompanyDetailsForm.jsx";
+import RegistrationScreen from './pages/RegistrationScreen/RegistrationScreen';
+import CompanyInfoUpdate from './pages/CompanyInfoUpdate/CompanyInfoUpdate';
 
 export default function App() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Provjera da li postoji userData u localStorage
+        const userData = localStorage.getItem('userData');
+
+        // Ako postoji userData, preusmjeri na /companydetails
+        if (userData) {
+            console.log(userData);
+            navigate('/companydetails', { replace: false});
+        }
+    }, [navigate]);
+
     return (
         <>
             <Routes>
                 <Route path='/' element={<LoginScreen />} />
                 <Route path='/registration' element={<RegistrationScreen />} />
                 <Route path='/login' element={<LoginScreen />} />
-                <Route path='/companydetails' element={<CompanyDetailsForm />} />
+                <Route path='/companydetails' element={<CompanyInfoUpdate />} />
             </Routes>
         </>
     );
