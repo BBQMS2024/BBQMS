30cca6c294baaea62d50d0f00d98ec4e99c1c218
ebabic1 <ebabic1@etf.unsa.ba>
2024-04-02 21:26:17 +0200

    rename dto

--- backend/src/main/java/ba/unsa/etf/si/bbqms/ws/models/ServiceResponseDto.java
+++ backend/src/main/java/ba/unsa/etf/si/bbqms/ws/models/ServiceResponseDto.java
@@ -3,9 +3,9 @@ package ba.unsa.etf.si.bbqms.ws.models;
 import ba.unsa.etf.si.bbqms.domain.Service;
 import ba.unsa.etf.si.bbqms.domain.Tenant;
 
-public record ServiceDto(long id, String name, Tenant tenant) {
-    public static ServiceDto fromEntity(final Service service) {
-        return new ServiceDto(
+public record ServiceResponseDto(long id, String name, Tenant tenant) {
+    public static ServiceResponseDto fromEntity(final Service service) {
+        return new ServiceResponseDto(
                 service.getId(),
                 service.getName(),
                 service.getTenant()
