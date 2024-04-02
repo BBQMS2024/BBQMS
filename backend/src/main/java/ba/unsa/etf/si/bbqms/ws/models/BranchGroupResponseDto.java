3d1cf289fec1faf68ba34fc0f73f9b6603959dc7
ebabic1 <ebabic1@etf.unsa.ba>
2024-04-02 21:31:58 +0200

    refactor dto usages

--- backend/src/main/java/ba/unsa/etf/si/bbqms/ws/models/BranchGroupResponseDto.java
+++ backend/src/main/java/ba/unsa/etf/si/bbqms/ws/models/BranchGroupResponseDto.java
@@ -7,14 +7,14 @@ import ba.unsa.etf.si.bbqms.domain.Service;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record BranchGroupResponseDto (Long id, String name, Set<ServiceDto> services, Set<BranchDto> branches) {
+public record BranchGroupResponseDto (Long id, String name, Set<ServiceResponseDto> services, Set<BranchDto> branches) {
     public static BranchGroupResponseDto fromEntity(final BranchGroup branchGroup) {
         Set<Branch> branchSet = branchGroup.getBranches();
         Set<Service> serviceSet = branchGroup.getServices();
         return new BranchGroupResponseDto(
                 branchGroup.getId(),
                 branchGroup.getName(),
-                serviceSet.stream().map(ServiceDto::fromEntity).collect(Collectors.toSet()),
+                serviceSet.stream().map(ServiceResponseDto::fromEntity).collect(Collectors.toSet()),
                 branchSet.stream().map(BranchDto::fromEntity).collect(Collectors.toSet())
         );
     }
--- backend/src/main/java/ba/unsa/etf/si/bbqms/ws/models/TellerStationResponseDto.java
+++ backend/src/main/java/ba/unsa/etf/si/bbqms/ws/models/TellerStationResponseDto.java
@@ -6,13 +6,13 @@ import ba.unsa.etf.si.bbqms.domain.TellerStation;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record TellerStationResponseDto(long id, String name, Set<ServiceDto> services) {
+public record TellerStationResponseDto(long id, String name, Set<ServiceResponseDto> services) {
     public static TellerStationResponseDto fromEntity(final TellerStation tellerStation) {
         final Set<Service> serviceSet = tellerStation.getServices();
         return new TellerStationResponseDto(
                 tellerStation.getId(),
                 tellerStation.getName(),
-                serviceSet.stream().map(ServiceDto::fromEntity).collect(Collectors.toSet())
+                serviceSet.stream().map(ServiceResponseDto::fromEntity).collect(Collectors.toSet())
         );
     }
 
