c915d228c9f13647548b15359140db2df3b677ff
nejradilovic <nadilovic2@etf.unsa.ba>
2024-04-03 22:58:41 +0200

    Changes in StationServiceScreen

--- admin-app/src/pages/StationServiceScreen/StationServiceScreen.jsx
+++ admin-app/src/pages/StationServiceScreen/StationServiceScreen.jsx
@@ -8,25 +8,24 @@ const StationServiceScreen = () => {
     const location = useLocation();
     const [showModal, setShowModal] = useState(false);
     const [deleteConfirmation, setDeleteConfirmation] = useState(false);
-    const [branches, setBranches] = useState([]);
-    const [selectedBranch, setSelectedBranch] = useState(null);
+    const [stations, setStations] = useState([]);
     const [selectedStation, setSelectedStation] = useState(null);
     const [selectedServices, setSelectedServices] = useState([]);
     const [availableServices, setAvailableServices] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
-        async function fetchBranches() {
+        async function fetchStations() {
             try {
                 const tenantCode = location.pathname.split('/')[1];
-                const response = await fetchData(`http://localhost:8080/api/v1/branches/${tenantCode}`, 'GET');
+                const response = await fetchData(`http://localhost:8080/api/v1/stations/${tenantCode}`, 'GET');
                 if (response.success) {
-                    setBranches(response.data);
+                    setStations(response.data);
                 } else {
-                    console.error('Error fetching branches:', response.error);
+                    console.error('Error fetching stations:', response.error);
                 }
             } catch (error) {
-                console.error('Error fetching branches:', error);
+                console.error('Error fetching stations:', error);
             }
         }
 
@@ -44,43 +43,84 @@ const StationServiceScreen = () => {
             }
         }
 
-        fetchBranches();
+        fetchStations();
         fetchAvailableServices();
     }, [location]);
+
+    useEffect(() => {
+        setSelectedServices(selectedStation ? selectedStation.services : []);
+    }, [selectedStation]);
 
     const confirmDeleteStation = () => {
         // Implement station deletion logic here
     };
 
-    const saveServiceToStation = async () => {
+    const saveServiceToStation = async (service) => {
         try {
             const tenantCode = location.pathname.split('/')[1];
-            const promises = selectedServices.map(async (service) => {
-                const response = await fetchData(`http://localhost:8080/api/v1/stations/${tenantCode}/${selectedStation.id}/services/${service.id}`, 'PUT');
-                if (!response.success) {
-                    console.error('Error adding service to station:', response.error);
-                }
-            });
-            await Promise.all(promises);
-            console.log('Services added to station successfully');
+            const response = await fetchData(`http://localhost:8080/api/v1/stations/${tenantCode}/${selectedStation.id}/services/${service.id}`, 'PUT');
+            if (response.success) {
+                console.log('Service added to station successfully');
+
+                // Ažurirajte lokalno stanje stanicama kako biste odmah prikazali dodani servis
+                const updatedStations = stations.map(station => {
+                    if (station.id === selectedStation.id) {
+                        return {
+                            ...station,
+                            services: [...station.services, service]
+                        };
+                    }
+                    return station;
+                });
+                setStations(updatedStations);
+            } else {
+                console.error('Error adding service to station:', response.error);
+            }
         } catch (error) {
             console.error('Error adding service to station:', error);
         }
     };
 
-    const removeSelectedService = (index) => {
-        const updatedServices = [...selectedServices];
-        updatedServices.splice(index, 1);
+    const removeServiceFromStation = async (serviceId) => {
+        try {
+            const tenantCode = location.pathname.split('/')[1];
+            const response = await fetchData(`http://localhost:8080/api/v1/stations/${tenantCode}/${selectedStation.id}/services/${serviceId}`, 'DELETE');
+            if (response.success) {
+                console.log('Service removed from station successfully');
+
+                // Ažurirajte lokalno stanje stanicama kako biste odmah prikazali uklonjeni servis
+                const updatedStations = stations.map(station => {
+                    if (station.id === selectedStation.id) {
+                        return {
+                            ...station,
+                            services: station.services.filter(service => service.id !== serviceId)
+                        };
+                    }
+                    return station;
+                });
+                setStations(updatedStations);
+            } else {
+                console.error('Error removing service from station:', response.error);
+            }
+        } catch (error) {
+            console.error('Error removing service from station:', error);
+        }
+    };
+
+    const removeSelectedService = (serviceId) => {
+        removeServiceFromStation(serviceId);
+        const updatedServices = selectedServices.filter(service => service.id !== serviceId);
         setSelectedServices(updatedServices);
     };
 
     const addService = (service) => {
+        saveServiceToStation(service);
         setSelectedServices([...selectedServices, service]);
     };
 
     const handleCloseModal = () => {
+        setSelectedServices(selectedStation ? selectedStation.services : []);
         setShowModal(false);
-        setSelectedServices([]); // Reset selected services when modal is closed
     };
 
     return (
@@ -92,66 +132,94 @@ const StationServiceScreen = () => {
                     <th>Station ID</th>
                     <th>Station Name</th>
                     <th>Services</th>
-                    <th>Actions</th>
+                    <th>Service Action</th>
+                    <th>Displays</th>
+                    <th>Display Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                {branches.map((branch, index) => (
-                    branch.tellerStations.map((station, stationIndex) => (
-                        <tr key={`${index}-${stationIndex}`}>
-                            <td>{station.id}</td>
-                            <td>{station.name}</td>
-                            <td>
-                                {station.services && station.services.length > 0 ? (
-                                    station.services.map((service, serviceIndex) => (
-                                        <span key={serviceIndex} className="badge bg-secondary m-1" style={{ display: 'inline-flex', alignItems: 'center' }}>
-                                                {service.name}
-                                            </span>
-                                    ))
-                                ) : (
-                                    <span>No services</span>
-                                )}
-                            </td>
-                            <td>
-                                <Button variant="primary" style={{ backgroundColor: '#548CA8', color: 'white', borderColor: '#548CA8' }} onClick={() => { setShowModal(true); setSelectedBranch(branch); setSelectedStation(station); }}>Add</Button>
-                            </td>
-                        </tr>
-                    ))
+                {stations.map((station, index) => (
+                    <tr key={index}>
+                        <td>{station.id}</td>
+                        <td>{station.name}</td>
+                        <td>
+                            {station.services && station.services.length > 0 ? (
+                                station.services.map((service, serviceIndex) => (
+                                    <span key={serviceIndex} style={{marginRight: '5px'}}>
+                                            {serviceIndex > 0 && ', '}
+                                        {service.name}
+                                        </span>
+                                ))
+                            ) : (
+                                <span>No services</span>
+                            )}
+                        </td>
+                        <td>
+                            <Button variant="primary"
+                                    style={{backgroundColor: '#548CA8', color: 'white', borderColor: '#548CA8'}}
+                                    onClick={() => {
+                                        setShowModal(true);
+                                        setSelectedStation(station);
+                                    }}>Edit</Button>
+                        </td>
+                        <td>
+                            {station.services && station.services.length > 0 ? (
+                                station.services.map((service, serviceIndex) => (
+                                    <span key={serviceIndex} style={{marginRight: '5px'}}>
+                                            {serviceIndex > 0 && ', '}
+                                        {service.name}
+                                        </span>
+                                ))
+                            ) : (
+                                <span>No displays</span>
+                            )}
+                        </td>
+                        <td>
+                            <Button variant="primary"
+                                    style={{backgroundColor: '#548CA8', color: 'white', borderColor: '#548CA8'}}
+                                    onClick={() => {
+                                        setShowModal(true);
+                                        setSelectedStation(station);
+                                    }}>Edit</Button>
+                        </td>
+                    </tr>
                 ))}
                 </tbody>
             </Table>
 
             <Modal show={showModal} onHide={handleCloseModal}>
-                <Modal.Header closeButton style={{ backgroundColor: '#334257', color: 'white' }}>
+                <Modal.Header closeButton style={{backgroundColor: '#334257', color: 'white'}}>
                     <Modal.Title>Add Service to Station</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <p><strong>Teller Station:</strong> {selectedStation && selectedStation.name}</p>
                     <div>
-                    {selectedServices.map((service, index) => (
-                            <span key={index} className="badge bg-secondary m-1" style={{ display: 'inline-flex', alignItems: 'center' }}>
-                                {service.name}
-                                <Button variant="danger" size="sm" style={{ marginLeft: '5px', padding: '2px 5px', backgroundColor: '#dc3545', borderColor: '#dc3545' }} onClick={() => removeSelectedService(index)}>X</Button>
-                            </span>
-                        ))}
+                        <div style={{marginBottom: '10px'}}>
+                            <strong>Selected Services:</strong>
+                            {selectedServices.map((service, index) => (
+                                <span key={index} className="badge bg-secondary m-1" style={{ display: 'inline-flex', alignItems: 'center' }}>
+                                    {service.name}
+                                    <Button variant="danger" size="sm" style={{ marginLeft: '5px', padding: '2px 5px', backgroundColor: '#dc3545', borderColor: '#dc3545' }} onClick={() => removeSelectedService(service.id)}>X</Button>
+                                </span>
+                            ))}
+                        </div>
+                        <Form>
+                            <Form.Group controlId="formGroupService" className="mb-3">
+                                <DropdownButton
+                                    title={'Select Service'}
+                                    variant="btn btn-outline-secondary"
+                                >
+                                    {availableServices.map((service, index) => (
+                                        <Dropdown.Item key={index} onClick={() => addService(service)}>{service.name}</Dropdown.Item>
+                                    ))}
+                                </DropdownButton>
+                            </Form.Group>
+                        </Form>
+                        {errorMessage && <p className="text-danger">{errorMessage}</p>}
                     </div>
-                    <Form>
-                        <Form.Group controlId="formGroupService" className="mb-3">
-                            <DropdownButton
-                                title={'Select Service'}
-                                variant="btn btn-outline-secondary"
-                            >
-                                {availableServices.map((service, index) => (
-                                    <Dropdown.Item key={index} onClick={() => addService(service)}>{service.name}</Dropdown.Item>
-                                ))}
-                            </DropdownButton>
-                        </Form.Group>
-                    </Form>
-                    {errorMessage && <p className="text-danger">{errorMessage}</p>}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
-                    <Button variant="primary" style={{ backgroundColor: '#548CA8', color: 'white', borderColor: '#548CA8' }} onClick={() => { handleCloseModal(); saveServiceToStation(); }}>Save</Button>
                 </Modal.Footer>
             </Modal>
 
