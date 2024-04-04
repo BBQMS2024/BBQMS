812d2737d7b9b85c1fa7e4867abaaae74d4557cd
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2024-04-04 12:25:43 +0200

    remove settings button

--- admin-app/src/components/Header/Header.jsx
+++ admin-app/src/components/Header/Header.jsx
@@ -17,10 +17,6 @@ export default function Header() {
         navigate('/');
     }
 
-    function handleSettings(){
-        navigate('/adminprofile')
-    }
-
     return (
         <header className="main-header">
 
@@ -30,14 +26,9 @@ export default function Header() {
             </div>
             <div className="header-logout">
                 { !!user && (
-                    <>
                         <button className="header-logout-btn" onClick={ handleLogout }>
                         Logout
                         </button>
-                        <button className="header-logout-btn" onClick={ handleSettings }>
-                        Settings
-                    </button>
-                    </>
                 ) }
 
             </div>
