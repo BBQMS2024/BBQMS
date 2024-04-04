782d709749f67522f487fdcf823c29d32b762d88
Amina <aminahromic@gmail.com>
2024-04-04 02:52:20 +0200

    Added navigation

--- admin-app/src/components/HomePageCard/HomePageCard.jsx
+++ admin-app/src/components/HomePageCard/HomePageCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useNavigate, useParams } from 'react-router-dom';
 import './HomePageCard.css'
 
-export default function HomePageCard( {title, backgroundColor, buttonColor} ) {
+export default function HomePageCard( {title, backgroundColor, buttonColor, url} ) {
+    const navigate = useNavigate();
     return (
         <div>
             <div className="card card-hp" style={{border: '1px solid #334257', backgroundColor: backgroundColor, height: '200px'}}>
@@ -10,7 +12,7 @@ export default function HomePageCard( {title, backgroundColor, buttonColor} ) {
                     <p className="card-text card-text-hp" style={{color: 'white'}}></p>
                     <div className="button-container-hp">
                         <a href="#" className="btn btn-primary button-hp"
-                           style={{backgroundColor: buttonColor}}>Open</a>
+                           style={{backgroundColor: buttonColor}} onClick ={ () => navigate(url) } >Open</a>
                     </div>
                 </div>
             </div>
