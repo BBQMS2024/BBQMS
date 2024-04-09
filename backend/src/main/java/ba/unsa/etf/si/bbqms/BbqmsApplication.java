9a124c2c3b0960d2631042962da25db1a156bd18
Emir Kapic <emirkapic0@gmail.com>
2024-04-09 20:00:19 +0200

    add missing annotation

--- backend/src/main/java/ba/unsa/etf/si/bbqms/BbqmsApplication.java
+++ backend/src/main/java/ba/unsa/etf/si/bbqms/BbqmsApplication.java
@@ -3,9 +3,11 @@ package ba.unsa.etf.si.bbqms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
 public class BbqmsApplication {
 
 	public static void main(String[] args) {
