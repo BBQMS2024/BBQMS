03772723b6ef4844b033c65fe44ecd1b8dcb7f08
BenjaminKadic <115993982+BenjaminKadic@users.noreply.github.com>
2024-04-14 15:43:57 +0200

    Setup testing framework (#32)
    Co-authored-by: Emir Kapic <emirkapic0@gmail.com>

--- backend/src/test/java/ba/unsa/etf/si/bbqms/unit/RoleServiceTest.java
+++ backend/src/test/java/ba/unsa/etf/si/bbqms/unit/RoleServiceTest.java
@@ -1,4 +1,4 @@
-package ba.unsa.etf.si.bbqms;
+package ba.unsa.etf.si.bbqms.unit;
 
 import ba.unsa.etf.si.bbqms.auth_service.implementation.DefaultRoleService;
 import ba.unsa.etf.si.bbqms.domain.Role;
--- backend/src/test/java/ba/unsa/etf/si/bbqms/unit/TenantServiceTest.java
+++ backend/src/test/java/ba/unsa/etf/si/bbqms/unit/TenantServiceTest.java
@@ -1,4 +1,4 @@
-package ba.unsa.etf.si.bbqms;
+package ba.unsa.etf.si.bbqms.unit;
 
 import ba.unsa.etf.si.bbqms.domain.Tenant;
 import ba.unsa.etf.si.bbqms.domain.TenantLogo;
