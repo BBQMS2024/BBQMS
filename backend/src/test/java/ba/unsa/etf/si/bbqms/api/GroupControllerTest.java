be999d1dbd120b667d9a80616b8c8af470e52861
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-04-24 22:56:58 +0200

    add cases for bad requests

--- backend/src/test/java/ba/unsa/etf/si/bbqms/api/GroupControllerTest.java
+++ backend/src/test/java/ba/unsa/etf/si/bbqms/api/GroupControllerTest.java
@@ -18,8 +18,10 @@ import static org.hamcrest.Matchers.greaterThan;
 public class GroupControllerTest extends BaseSetup {
     private int branchGroupId;
     private final String tenantCode = "DFLT";
+    private final String invalidTenantCode = ".";
+
     @BeforeEach
-    public void Should_CreateBranchGroupSuccessfully_When_ValidParametersProvided() {
+    public void createBranchGroup_ValidNameBranchIdsAndServiceIds_ReturnsOkResponse() {
         final String name = "Dummy Branch Group";
         final Set<Long> branchIds = Set.of(2L);
         final Set<Long> serviceIds = Set.of(1L);
@@ -45,7 +47,7 @@ public class GroupControllerTest extends BaseSetup {
     }
 
     @AfterEach
-    public void Should_DeleteBranchGroupSuccessfully_When_GivenValidBranchGroupId() {
+    public void deleteBranchGroup_ValidBranchGroupId_ReturnsOkResponse() {
         given()
                 .pathParam("tenantCode", tenantCode)
                 .pathParam("branchGroupId", branchGroupId)
@@ -57,7 +59,7 @@ public class GroupControllerTest extends BaseSetup {
     }
 
     @Test
-    public void Should_RetrieveAllBranchGroupsFromTenant_When_ValidTenantCodeIsProvided() {
+    public void getAllBranchGroups_ValidTenantCode_ReturnsOkResponse() {
         given()
                 .pathParam("tenantCode", tenantCode)
             .when()
@@ -69,7 +71,64 @@ public class GroupControllerTest extends BaseSetup {
     }
 
     @Test
-    public void Should_AddServiceToBranchGroup_When_ServiceIdIsProvided() {
+    public void addBranchGroup_NonExistentService_ReturnsBadRequest() {
+        final String name = "Dummy Branch Group";
+        final Set<Long> branchIds = Set.of(2L);
+        final Set<Long> serviceIds = Set.of(-1L);
+
+        final BranchGroupCreateDto newBranchGroupRequest =
+                new BranchGroupCreateDto(name, serviceIds, branchIds);
+
+       given()
+                .pathParam("tenantCode", tenantCode)
+                .body(newBranchGroupRequest)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .post("api/v1/groups/{tenantCode}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void addBranchGroup_InvalidTenantCode_ReturnsBadRequest() {
+        final String name = "Dummy Branch Group";
+        final Set<Long> branchIds = Set.of(2L);
+        final Set<Long> serviceIds = Set.of(1L);
+
+        final BranchGroupCreateDto newBranchGroupRequest =
+                new BranchGroupCreateDto(name, serviceIds, branchIds);
+
+        given()
+                .pathParam("tenantCode", invalidTenantCode)
+                .body(newBranchGroupRequest)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .post("api/v1/groups/{tenantCode}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void addBranchGroup_NonExistentBranch_ReturnsBadRequest() {
+        final String name = "Dummy Branch Group";
+        final Set<Long> branchIds = Set.of(-1L);
+        final Set<Long> serviceIds = Set.of(2L);
+
+        final BranchGroupCreateDto newBranchGroupRequest =
+                new BranchGroupCreateDto(name, serviceIds, branchIds);
+
+        given()
+                .pathParam("tenantCode", tenantCode)
+                .body(newBranchGroupRequest)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .post("api/v1/groups/{tenantCode}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void addServiceToBranchGroup_ValidServiceId_ReturnsOkResponse() {
         final int serviceId = 2;
 
         given()
@@ -85,7 +144,37 @@ public class GroupControllerTest extends BaseSetup {
     }
 
     @Test
-    public void Should_AddBranchToBranchGroup_When_BranchIdIsProvided() {
+    public void addServiceToBranchGroup_NonExistentServiceId_ReturnsBadRequest() {
+        final int serviceId = -1;
+
+        given()
+                .pathParam("tenantCode", tenantCode)
+                .pathParam("groupId", branchGroupId)
+                .pathParam("serviceId", serviceId)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .put("api/v1/groups/{tenantCode}/{groupId}/services/{serviceId}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void addServiceToBranchGroup_InvalidTenantCode_ReturnsBadRequest() {
+        final int serviceId = 2;
+
+        given()
+                .pathParam("tenantCode", invalidTenantCode)
+                .pathParam("groupId", branchGroupId)
+                .pathParam("serviceId", serviceId)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .put("api/v1/groups/{tenantCode}/{groupId}/services/{serviceId}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void addBranchToBranchGroup_ValidBranchId_ReturnsOkResponse() {
         final int branchId = 1;
 
         given()
@@ -101,7 +190,37 @@ public class GroupControllerTest extends BaseSetup {
     }
 
     @Test
-    public void Should_UpdateBranchGroupName_When_NameIsProvided() {
+    public void addBranchToBranchGroup_NonExistentBranchId_ReturnsBadRequest() {
+        final int branchId = -1;
+
+        given()
+                .pathParam("tenantCode", tenantCode)
+                .pathParam("groupId", branchGroupId)
+                .pathParam("serviceId", branchId)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .put("api/v1/groups/{tenantCode}/{groupId}/branches/{serviceId}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void addBranchToBranchGroup_InvalidTenantCode_ReturnsBadRequest() {
+        final int branchId = 1;
+
+        given()
+                .pathParam("tenantCode", invalidTenantCode)
+                .pathParam("groupId", branchGroupId)
+                .pathParam("serviceId", branchId)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .put("api/v1/groups/{tenantCode}/{groupId}/branches/{serviceId}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void updateBranchGroupName_Name_ReturnsOkResponse() {
         final String newName = "New Branch Group";
 
         final BranchGroupUpdateDto branchGroupUpdateDto =
@@ -120,7 +239,25 @@ public class GroupControllerTest extends BaseSetup {
     }
 
     @Test
-    public void Should_RemoveServiceFromBranchGroup_WhenServiceIdIsProvided() {
+    public void updateBranchGroupName_InvalidTenantCode_ReturnsBadRequest() {
+        final String newName = "New Branch Group";
+
+        final BranchGroupUpdateDto branchGroupUpdateDto =
+                new BranchGroupUpdateDto(newName);
+
+        given()
+                .pathParam("tenantCode", invalidTenantCode)
+                .pathParam("branchGroupId", branchGroupId)
+                .body(branchGroupUpdateDto)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .put("api/v1/groups/{tenantCode}/{branchGroupId}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void removeServiceFromBranchGroup_ValidServiceId_ReturnsOkResponse() {
         final int serviceId = 1;
 
         given()
@@ -136,7 +273,37 @@ public class GroupControllerTest extends BaseSetup {
     }
 
     @Test
-    public void Should_RemoveBranchFromBranchGroup_WhenBranchIdIsProvided() {
+    public void removeServiceFromBranchGroup_InvalidServiceId_ReturnsBadRequest() {
+        final int serviceId = -1;
+
+        given()
+                .pathParam("tenantCode", tenantCode)
+                .pathParam("branchGroupId", branchGroupId)
+                .pathParam("serviceId", serviceId)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .delete("api/v1/groups/{tenantCode}/{branchGroupId}/services/{serviceId}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void removeServiceFromBranchGroup_InvalidTenantCode_ReturnsBadRequest() {
+        final int serviceId = 1;
+
+        given()
+                .pathParam("tenantCode", invalidTenantCode)
+                .pathParam("branchGroupId", branchGroupId)
+                .pathParam("serviceId", serviceId)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .delete("api/v1/groups/{tenantCode}/{branchGroupId}/services/{serviceId}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void removeBranchFromBranchGroup_ValidBranchId_ReturnsOkResponse() {
         final int branchId = 2;
 
         given()
@@ -149,5 +316,35 @@ public class GroupControllerTest extends BaseSetup {
             .then()
                 .statusCode(200)
                 .body("branches.id", not(hasItem(branchId)));
+    }
+
+    @Test
+    public void removeBranchFromBranchGroup_InvalidBranchId_ReturnsBadRequest() {
+        final int branchId = -1;
+
+        given()
+                .pathParam("tenantCode", tenantCode)
+                .pathParam("branchGroupId", branchGroupId)
+                .pathParam("branchId", branchId)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .delete("api/v1/groups/{tenantCode}/{branchGroupId}/branches/{branchId}")
+            .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public void removeBranchFromBranchGroup_InvalidTenantCode_ReturnsBadRequest() {
+        final int branchId = 1;
+
+        given()
+                .pathParam("tenantCode", invalidTenantCode)
+                .pathParam("branchGroupId", branchGroupId)
+                .pathParam("branchId", branchId)
+            .when()
+                .header("Authorization", "Bearer " + generateSuperAdminToken())
+                .delete("api/v1/groups/{tenantCode}/{branchGroupId}/branches/{branchId}")
+            .then()
+                .statusCode(400);
     }
 }
