7b352879a83c221cf7f680c63a57e6503dd1cea3
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2024-05-02 14:29:10 +0200

    implement show queues for teller station

---
+++ teller-app/src/pages/ShowQueuesForTellerPage/ShowQueuesForTellerPage.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { fetchData } from '../../fetching/Fetch.js'
+import { SERVER_URL } from '../../constants.js';
+
+const styles = {
+    stationRow: {
+        backgroundColor: "#ADD8E6",
+    }
+};
+
+const ShowQueuesForTellerPage = () =>{
+    const [tickets, setTickets] = useState([]);
+    const [tellerStationId, setTellerStationId] = useState(0);
+    const url = `${SERVER_URL}/api/v1/`;
+
+    useEffect(() =>{
+        fetchTicketsForTellerStation();
+    }, []);
+
+    const fetchTicketsForTellerStation = async () => {
+        try{
+            const response = await fetchData(`${url}stations/${tellerStationId}/tickets`, 'GET');
+    
+            if(response.success){
+                const sortedTickets = response.data.sort((a, b) => a.number - b.number);
+                setTickets(sortedTickets);
+            }else{
+                console.error('Error fethcing tickets:', response.error);
+            }
+        }catch(error){
+            console.log("Error fetching tickets for tellerStation:", error);
+        }
+    };
+
+    const advanceQueue = async () =>{
+        try{
+            const response = await fetchData(`${url}teller/advance-queue/${tellerStationId}`, 'POST');
+            if(response.success){
+                await fetchTicketsForTellerStation();
+            }else{
+                console.error('Error advancing queue:', response.error);
+            }
+        }catch(error){
+            console.log("Error advancing queue:", error);
+        }
+    }
+
+    return (
+        <div className="text-center mt-5">
+            <h2>Queues</h2>
+            <Button
+                variant="primary"
+                style={ { backgroundColor: '#548CA8', color: 'white', borderColor: '#548CA8' } }
+                onClick={advanceQueue}
+            >
+                Advance Queue
+            </Button>
+            <div style={ { marginTop: '20px' } }>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Ticket number</th>
+                            <th>Service</th>
+                            <th>Creation at</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {tickets.map((ticket, index) => {
+                            const createdAt = new Date(ticket.createdAt);
+                            return (
+                                <tr key={index}>
+                                    <td style={ticket.station?.id === tellerStationId ? styles.stationRow : {}}>{ticket.number}</td>
+                                    <td>{ticket.service.name}</td>
+                                    <td>{createdAt.toLocaleString()}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </Table>
+            </div>
+        </div>
+    )
+
+}
+
+export default ShowQueuesForTellerPage;
