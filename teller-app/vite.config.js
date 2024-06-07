8eaaf3d7846ee9c46491f9ea7f8f228e25ee1eee
Emir Kapic <emirkapic0@gmail.com>
2024-06-07 17:01:56 +0200

    .

--- teller-app/vite.config.js
+++ teller-app/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5000
+  },
+  preview: {
+    host: true,
+    port: 5000
+  }
 })
